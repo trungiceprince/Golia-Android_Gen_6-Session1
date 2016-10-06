2e0e6c69cf61f2affa2d51658e3bc9dd1590ff1b
GOLIA9x <trungiceprince@gmail.com>
2016-10-06 22:39:59 +0700

    no message

---
+++ HW-session2/season2/src/program.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Admin on 10/2/2016.
+ */
+public class program {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        Gamewindow gamewindow = new Gamewindow();
+    }
+}
